0f9fbb1f48dbb94c142f3dace8e85c3bfd5a4755
Grace <grace.wambugu@strathmore.edu>
2024-06-10 13:40:57 +0300

    new update

--- index.php
+++ index.php
@@ -12,24 +12,8 @@
  
 
  
-<div class="menu_bar">
-    <h1 class="logo">Bloom <span>Berg.</span></h1>
-    <ul>
-        <li><a href="about us.html">About</a></li>
-        <li><a href="Contact us.html">Contact us</a></li>
-        <li><a href="export.html">Export</a></li>
-        <li><a href="kidszone.html">Kids Zone</a></li>
-        <li>
-            <a href="Sign in.html">Sign in <i class="fas fa-caret-down"></i></a>
-            <div class="dropdown_menu">
-                <ul>
-                    <li><a href="signin.html">Administrator</a></li>
-                    <li><a href="signup.html">Staff</a></li>
-                </ul>
-            </div>
-        </li>
-    
-</div>
+
+<?include_once("template/navigation.php");?>
 <div class ="background"></div>
 
 
---
+++ template/navigation.php
@@ -0,0 +1,18 @@
+<div class="menu_bar">
+    <h1 class="logo">Bloom <span>Berg.</span></h1>
+    <ul>
+        <li><a href="about us.php">About</a></li>
+        <li><a href="Contact us.html">Contact us</a></li>
+        <li><a href="export.html">Export</a></li>
+        <li><a href="kidszone.html">Kids Zone</a></li>
+        <li>
+            <a href="Sign in.html">Sign in <i class="fas fa-caret-down"></i></a>
+            <div class="dropdown_menu">
+                <ul>
+                    <li><a href="signin.html">Administrator</a></li>
+                    <li><a href="signup.html">Staff</a></li>
+                </ul>
+            </div>
+        </li>
+    
+</div>
