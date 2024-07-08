dddf02942ad526b7aefccac3c1169f7e0775a429
Grace <grace.wambugu@strathmore.edu>
2024-07-08 05:01:45 +0300

    signup insertion

--- signup.php
+++ signup.php
@@ -1,3 +1,10 @@
+
+<?php 
+include 'connect.php'; 
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,9 +34,13 @@
 
     <form action="action_page.php">
   <div class="container">
+
     <h1>Register</h1>
     <p>Please fill in this form to create an account.</p>
     <hr>
+    
+    <label for="name"><b>Name</b></label>
+    <input type="text" placeholder="Enter you name" name="name" id="name" required>
 
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
