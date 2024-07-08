9da075e66305050bbfcaed827b4a09df1ce408b8
Grace <grace.wambugu@strathmore.edu>
2024-07-08 06:09:31 +0300

    display

--- display.php
+++ display.php
@@ -17,30 +17,26 @@
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Password</th>
+      <th scope="col">Repeat Password</th>
+      <th scope="col">Operations</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+
+  <?php 
+
+  $sql="Select *from `signup`";
+  $result=mysqli_query($con,$sql);
+  if($result){
+    $row=mysqli_fetch_assoc($result);
+    echo$ $row ['name'];
+  }
+  ?>
+ 
   </tbody>
 </table>
 
